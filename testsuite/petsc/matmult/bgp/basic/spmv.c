9d38fe9562d178c64d5e1f11eb11d2af7bb432b5
Albert Hartono <albert.hartono@gmail.com>
2008-08-08 18:06:16 +0000

    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@246 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- testsuite/petsc/matmult/bgp/basic/spmv.c
+++ testsuite/petsc/matmult/bgp/basic/spmv.c
@@ -1,9 +1,9 @@
 /*@ begin PerfTuning (  
  def build { 
    arg build_command = 'mpixlc_r -O3 -qsmp=omp:noauto -qstrict';
-   arg batch_command = 'qsub -n 4 -t 5 -q short --env "BG_MAXALIGNEXP=0"';
+   arg batch_command = 'qsub -n 64 -t 5 -q short --env "BG_MAXALIGNEXP=0"';
    arg status_command = 'qstat';
-   arg num_procs = 4;
+   arg num_procs = 64;
  }
 
  def performance_counter {
@@ -12,16 +12,16 @@
  }
 
  def performance_params {
-   param UNROLL_FAC_OUT[] = [1];
-   param UNROLL_FAC_IN[] = [2];
+   param UNROLL_FAC_OUT[] = [1,2,3,4];
+   param UNROLL_FAC_IN[] = [1,2,3,4,5,6,7,8];
    param N_THREADS[] = [1];
-   param SIMD_TYPE[] = ['xlc'];
+   param SIMD_TYPE[] = ['none','xlc'];
 
    constraint simd_unroll_factor = (SIMD_TYPE=='none' or UNROLL_FAC_IN%2==0);
  }
 
  def input_params {
-   param NROWS[] = [1000];
+   param NROWS[] = [700,1200,10000];
    param NCOLS[] = [25];
  }
  
