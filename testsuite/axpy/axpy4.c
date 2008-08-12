15f034251948ef6dd4e90e15dbe1b830171f02f3
Albert Hartono <albert.hartono@gmail.com>
2008-08-12 18:19:38 +0000

    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@274 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- testsuite/axpy/axpy4.c
+++ testsuite/axpy/axpy4.c
@@ -1,19 +1,21 @@
 
-/*@ begin PerfTuning (
-  import spec align_unroll;
-) @*/
+void axpy4(int n, double *y, double a1, double *x1, double a2, double *x2, double a3, double *x3,
+           double a4, double *x4) {
 
-  int i;
+    register int i;
 
-  /*@ begin Align (x1[],x2[],x3[],x4[],y[]) @*/
-  /*@ begin Loop ( 
-      transform Unroll(ufactor=UF) 
-        for (i = 0; i <= n-1; i++)
-          y[i] = y[i] + a1*x1[i] + a2*x2[i] + a3*x3[i] + a4*x4[i];
-  ) @*/
-  for (i = 0; i < n; i++)
-     y[i] = y[i] + a1*x1[i] + a2*x2[i] + a3*x3[i] + a4*x4[i];
-  /*@ end @*/
-  /*@ end @*/
- 
+/*@ begin Align (x1[],x2[],x3[],x4[],y[]) @*/
+/*@ begin Loop (
+  transform Unroll(ufactor=20, parallelize=True)
+    for (i=0; i<=n-1; i++)
+      y[i]=y[i]+a1*x1[i]+a2*x2[i]+a3*x3[i]+a4*x4[i];
+      ) @*/
+
+    for (i=0; i<=n-1; i++)
+        y[i]=y[i]+a1*x1[i]+a2*x2[i]+a3*x3[i]+a4*x4[i];
+
 /*@ end @*/
+/*@ end @*/
+
+}
+
