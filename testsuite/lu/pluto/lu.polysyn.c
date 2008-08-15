100c14f00123fd030c7e8ca2f542a60c0743ee0f
hartono <hartono@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2008-08-15 21:21:15 +0000

    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@285 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- testsuite/lu/pluto/lu.polysyn.c
+++ testsuite/lu/pluto/lu.polysyn.c
@@ -1,52 +1,50 @@
 /*@ begin PerfTuning (         
   def build 
   { 
-    arg command = 'icc'; 
-    arg options = '-fast -openmp -I/usr/local/icc/include -lm'; 
+    arg build_command = 'icc -O3 -openmp -I/usr/local/icc/include -lm'; 
   } 
     
   def performance_counter          
   { 
-    arg method = 'basic timer'; 
     arg repetitions = 1; 
   }
 
   def performance_params 
   {
-    param PERM_A[] = [
-      ('i','j'),
-      #('j','i'),
-      ];
+#    param T1_1[] = [1,16,32,64,128];
+#    param T1_2[] = [1,16,32,64,128];
+#    param T1_3[] = [1,16,32,64,128];
+#    param T2_1[] = [1,4,8,16,32];
+#    param T2_2[] = [1,4,8,16,32];
+#    param T2_3[] = [1,4,8,16,32];
 
-#    param T1_1[] = [8,16,32,64,128];
-#    param T1_2[] = [8,16,32,64,128];
-#    param T1_3[] = [8,16,32,64,128];
-#    param T2_1[] = [1,2,4,8];
-#    param T2_2[] = [1,2,4,8];
-#    param T2_3[] = [1,2,4,8];
+    param T1_1[] = [64];
+    param T1_2[] = [256];
+    param T1_3[] = [64];
+    param T2_1[] = [1];
+    param T2_2[] = [1];
+    param T2_3[] = [1];
 
-    param T1_1[] = [32];
-    param T1_2[] = [16];
-    param T1_3[] = [32];
-    param T2_1[] = [2];
-    param T2_2[] = [8];
-    param T2_3[] = [2];
+    constraint c1 = (T1_1*T2_1<=1024 and T1_1*T2_1<=1024 and T1_1*T2_1<=1024);
+    constraint c2 = ((T1_1 == T1_3) and (T2_1 == T2_3));
 
-    constraint LU_tiles = ((T1_1 == T1_3) and (T2_1 == T2_3));
-#    constraint LU_tiles = ((T2_1 == T2_3));
+    param U1[] = [1];
+    param U2[] = [1];
+    param U3[] = [7];
 
-    param PERM_B[] = [
-      [0,1,2],
+    constraint c3 = (U1*U2*U3<=512);
+
+    param PERM[] = [
+      #[0,1,2],
       #[0,2,1],
-      #[2,0,1],
+      #[1,0,2],
+      #[1,2,0],
+      [2,0,1],
+      #[2,1,0],
       ];
-
-    param U1[] = [32];
-    param U2[] = [8];
-    param U3[] = [1];
 
     param PAR[] = [True];
-    param SCREP[] = [True];
+    param SCREP[] = [False];
     param IVEC[] = [True];
   }
 
@@ -54,17 +52,19 @@
   { 
     arg algorithm = 'Exhaustive'; 
 #    arg algorithm = 'Simplex'; 
-#    arg algorithm = 'Random'; 
-    arg time_limit = 5;
-#    arg total_runs = 2;
+#    arg time_limit = 5;
+#    arg total_runs = 1;
   } 
    
   def input_params 
   {
-    param N[] = [256];
-    decl in static double L[N][N] = 0; 
-    decl in static double U[N][N] = 0; 
-    decl out static double A[N][N+13] = random; 
+    param N[] = [1024];
+  }
+
+  def input_vars
+  {
+    arg decl_file = 'decl_code.h';
+    arg init_file = 'init_code.c';
   }
 ) @*/ 
 
@@ -76,30 +76,30 @@ register int newub_c1, newub_c2, newub_c3, newub_c4, newub_c5, newub_c6,
   newub_c7, newub_c8, newub_c9, newub_c10, newub_c11, newub_c12;
 
 
-/*@ begin PolySyn(
-  l1_tiles = [T1_1,T1_2,T1_3];
-  l2_tiles = [T2_1,T2_2,T2_3];
-  hotspot_permut = PERM_B;
+/*@ begin PolySyn(    
+  parallel = PAR;
+  tiles = [T1_1,T1_2,T1_3,T2_1,T2_2,T2_3];
+  permut = PERM;
   unroll_factors = [U1,U2,U3];
-  parallelize = PAR;
   scalar_replace = SCREP;
-  icc_vectorize = IVEC;
-) @*/
+  vectorize = IVEC;
+    
+  profiling_code = 'lu_profiling.c';
+  compile_cmd = 'gcc';
+  compile_opts = '-lm';
+  ) @*/
 
 /* pluto start (N) */
 for (k=0; k<=N-1; k++)
   {
     for (j=k+1; j<=N-1; j++)
       A[k][j] = A[k][j]/A[k][k];
-/*@ begin Loop(
-    transform Composite(permut = [PERM_A])
     for(i=k+1; i<=N-1; i++)
       for (j=k+1; j<=N-1; j++)
         A[i][j] = A[i][j] - A[i][k]*A[k][j];
-) @*/
-/*@ end @*/
   }
 /* pluto end */
+
 /*@ end @*/
 /*@ end @*/
 
