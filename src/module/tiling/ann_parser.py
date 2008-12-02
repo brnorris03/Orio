6d9934ec09071599b2e3fa987bee0214c09f4602
Albert Hartono <albert.hartono@gmail.com>
2008-12-02 20:34:59 +0000

    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@372 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- src/module/tiling/ann_parser.py
+++ src/module/tiling/ann_parser.py
@@ -47,30 +47,12 @@ class AnnParser:
         __oparenth_re = r'\s*\(\s*'
         __cparenth_re = r'\s*\)\s*'
 
-        # get the number of tiling levels
-        m = re.match(__num_re, text)
-        if not m:
-            m = re.match(__var_re, text)
-        if not m:
-            print 'error:Tiling: annotation syntax error: "%s"' % orig_text
-            sys.exit(1)
-        text = text[m.end():]
-        num_level = m.group(1)
-        num_level = self.__evalExp(num_level)
+        # initialize the data structure to store all tiling information
+        num_level = 1
+        tiling_table = {}
+        tiling_info = [num_level, tiling_table]
 
-        # check the semantic of the number of tiling levels
-        if not isinstance(num_level, int) or num_level <= 0:
-            print 'error:Tiling: the number of tiling levels must be a positive integer'
-            sys.exit(1)
-
-        # get a colon
-        m = re.match(__colon_re, text)
-        if not m:
-            print 'error:Tiling: annotation syntax error: "%s"' % orig_text
-            sys.exit(1)
-        text = text[m.end():]
-
-        # get the list of iterator names of the loops to be tiled
+        # get all iterator names of the loops to be tiled
         m = re.match(__oparenth_re, text)
         if not m:
             print 'error:Tiling: annotation syntax error: "%s"' % orig_text
@@ -90,19 +72,51 @@ class AnnParser:
             if not m:
                 print 'error:Tiling: annotation syntax error: "%s"' % orig_text
                 sys.exit(1)
-            iter_names.append(m.group(1))
+            iname = m.group(1)
+            if iname in iter_names:
+                print 'error:Tiling: repeated iterator name: "%s"' % iname
+                sys.exit(1)
+            iter_names.append(iname)
             itext = itext[m.end():]
             m = re.match(__comma_re, itext)
             if m:
                 itext = itext[m.end():]
 
-        # create a data structure to store the tiling information
-        tiling_table = {}
+        # insert all obtained iterator names into the tiling table
         for i in iter_names:
             tiling_table[i] = []
-        tiling_info = (num_level, tiling_table)
 
-        # check if the tile sizes are specified or not
+        # check if further parsing is needed
+        if (not text) or text.isspace():
+            return tiling_info
+        
+        # get a colon
+        m = re.match(__colon_re, text)
+        if not m:
+            print 'error:Tiling: annotation syntax error: "%s"' % orig_text
+            sys.exit(1)
+        text = text[m.end():]
+
+        # get the number of tiling levels
+        m = re.match(__num_re, text)
+        if not m:
+            m = re.match(__var_re, text)
+        if not m:
+            print 'error:Tiling: annotation syntax error: "%s"' % orig_text
+            sys.exit(1)
+        text = text[m.end():]
+        num_level = m.group(1)
+        num_level = self.__evalExp(num_level)
+
+        # check the semantic of the number of tiling levels
+        if not isinstance(num_level, int) or num_level <= 0:
+            print 'error:Tiling: the number of tiling levels must be a positive integer'
+            sys.exit(1)
+
+        # insert the obtained number of tiling levels into the tiling information
+        tiling_info[0] = num_level
+
+        # check if further parsing is needed
         if (not text) or text.isspace():
             return tiling_info
 
@@ -114,8 +128,8 @@ class AnnParser:
         text = text[m.end():]
 
         # get the tile sizes
-        tile_sizes = []
-        for tlevel in range(1,num_level+1):
+        tile_size_sets = []
+        for tlevel in range(0,num_level):
             m = re.match(__oparenth_re, text)
             if not m:
                 print 'error:Tiling: annotation syntax error: "%s"' % orig_text
@@ -130,7 +144,7 @@ class AnnParser:
             m = re.match(__comma_re, text)
             if m:
                 text = text[m.end():]
-            tile_sizes.append([])
+            tile_size_sets.append([])
             for iname in iter_names:
                 m = re.match(__num_re, itext)
                 if not m:
@@ -139,7 +153,7 @@ class AnnParser:
                     print 'error:Tiling: annotation syntax error: "%s"' % orig_text
                     sys.exit(1)
                 tsize = self.__evalExp(m.group(1))
-                tile_sizes[-1].append(tsize)
+                tile_size_sets[-1].append(tsize)
                 itext = itext[m.end():]
                 m = re.match(__comma_re, itext)
                 if m:
@@ -153,10 +167,10 @@ class AnnParser:
             print 'error:Tiling: annotation syntax error: "%s"' % orig_text
             sys.exit(1)
 
-        # update the tiling information with tile sizes
-        tile_sizes = zip(*tile_sizes)
+        # insert the obtained tile sizes into the tiling table
+        tile_size_sets = zip(*tile_size_sets)
         for i, iname in enumerate(iter_names):
-            tiling_table[iname].extend(tile_sizes[i])
+            tiling_table[iname].extend(tile_size_sets[i])
 
         # check the semantics of the tile sizes
         for iname in iter_names:
@@ -168,15 +182,12 @@ class AnnParser:
             for i, cur_t in enumerate(tsizes):
                 for j, next_t in enumerate(tsizes[i+1:]):
                     if cur_t % next_t != 0:
-                        print (('error:Tiling: level-%s tile size of %s must be divisible by ' +
-                               'level-%s tile size of %s') % (i+1, cur_t, i+1+j+1, next_t))
+                        print (('error:Tiling: level-%s tile size (i.e., %s) must be divisible by ' +
+                                'level-%s tile size of (i.e., %s)') % (i+1, cur_t, i+1+j+1, next_t))
                         sys.exit(1)
         
         # return the tiling information
         return tiling_info
-
-
-
 
 
 
