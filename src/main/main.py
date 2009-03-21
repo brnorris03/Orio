1b3d2b36281868ac08382ad41bb910af1906abbf
Boyana Norris <brnorris03@gmail.com>
2009-03-21 17:28:08 +0000

    - enabled the use of orcc, orf as compiler wrappers, e.g., orcc -vr gcc -O -g f1.c f2.c
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@457 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- src/main/main.py
+++ src/main/main.py
@@ -25,12 +25,6 @@ def start(argv, lang):
         print 'error: Language not supported at this time.'
         sys.exit(1)
 
-    # include the annotation tool in the Python's search path
-    abs_path = os.path.abspath(__file__)
-    cur_dir = os.path.dirname(abs_path)
-    base_dir,_ = os.path.split(cur_dir)
-    sys.path.append(base_dir)
-    
     # import other required Python packages
     import ann_parser, cmd_line_opts, opt_driver, tspec.tspec
 
@@ -40,70 +34,91 @@ def start(argv, lang):
     # need to be verbose?
     verbose = cline_opts.verbose
 
-    if verbose: print '\n====== START ORIO ======'
+    if not cline_opts.disable_orio:
+        if verbose: print '\n====== START ORIO ======'
 
-    # read source code
-    if verbose: print '\n----- begin reading the source file: %s -----' % cline_opts.src_filename
-    try:
-        f = open(cline_opts.src_filename, 'r')
-        src_code = f.read()
-        f.close()
-    except:
-        print 'error: cannot open file for reading: %s' % cline_opts.src_filename
-        sys.exit(1)
-    if verbose: print '----- finish reading the source file -----'
+        for srcfile, out_filename in cline_opts.src_filenames.items():
+            # read source code
+            if verbose: print '\n----- begin reading the source file: %s -----' % srcfile
+            try:
+                f = open(srcfile, 'r')
+                src_code = f.read()
+                f.close()
+            except:
+                print 'error: cannot open file for reading: %s' % srcfile
+                sys.exit(1)
+            if verbose: print '----- finish reading the source file -----'
+    
+            # obtain the mapping for performance tuning specifications
+            specs_map = {}
+            if cline_opts.spec_filename:
+                if verbose: print ('\n----- begin reading the tuning specification file: %s -----' %
+                                   cline_opts.spec_filename)
+                try:
+                    f = open(cline_opts.spec_filename, 'r')
+                    tspec_prog = f.read()
+                    f.close()
+                except:
+                    print 'error: cannot open file for reading: %s' % cline_opts.spec_filename
+                    sys.exit(1)
+                specs_map = tspec.tspec.TSpec().parseProgram(tspec_prog)
+                if verbose: print '----- finish reading the tuning specification -----'
+    
+            # parse the source code and return a sequence of code fragments
+            if verbose: print '\n----- begin parsing annotations -----'
+            cfrags = ann_parser.AnnParser().parse(src_code)
+            if verbose: print '----- finish parsing annotations -----'
+    
+            # perform optimizations based on information specified in the annotations
+            if verbose: print '\n----- begin optimizations -----'
+            odriver = opt_driver.OptDriver(specs_map, cline_opts, language=language)
+            optimized_code_seq = odriver.optimizeCodeFrags(cfrags, {}, True)
+            if verbose: print '----- finish optimizations -----'
+        
+            # remove all annotations from output
+            if cline_opts.erase_annot:
+                if verbose: print '\n----- begin removing annotations from output-----'
+                optimized_code_seq = [[ann_parser.AnnParser().removeAnns(c), i] \
+                                      for c, i in optimized_code_seq]
+                if verbose: print '----- finish removing annotations from output-----'
+    
+            # write output
+            if verbose: print '\n----- begin writing the output file(s) -----'
+            for optimized_code, input_params in optimized_code_seq:
+                if len(optimized_code_seq) > 1:
+                    path_name, ext = os.path.splitext(out_filename)
+                    suffix = ''
+                    for pname, pval in input_params:
+                        suffix += '_%s_%s' % (pname, pval)
+                    out_filename = ('%s%s' % (path_name, suffix)) + ext
+                if verbose: print '--> writing output to: %s' % out_filename
+                try:
+                    f = open(out_filename, 'w')
+                    f.write(optimized_code)
+                    f.close()
+                except:
+                    print 'error: cannot open file for writing: %s' % cline_opts.out_filename
+                    sys.exit(1)
+            if verbose: print '----- finish writing the output file(s) -----'
 
-    # obtain the mapping for performance tuning specifications
-    specs_map = {}
-    if cline_opts.spec_filename:
-        if verbose: print ('\n----- begin reading the tuning specification file: %s -----' %
-                           cline_opts.spec_filename)
-        try:
-            f = open(cline_opts.spec_filename, 'r')
-            tspec_prog = f.read()
-            f.close()
-        except:
-            print 'error: cannot open file for reading: %s' % cline_opts.spec_filename
-            sys.exit(1)
-        specs_map = tspec.tspec.TSpec().parseProgram(tspec_prog)
-        if verbose: print '----- finish reading the tuning specification -----'
+        if verbose: print '\n====== END ORIO ======'
 
-    # parse the source code and return a sequence of code fragments
-    if verbose: print '\n----- begin parsing annotations -----'
-    cfrags = ann_parser.AnnParser().parse(src_code)
-    if verbose: print '----- finish parsing annotations -----'
+        # ----- end of "if not cline_opts.disable_orio:" -----
 
-    # perform optimizations based on information specified in the annotations
-    if verbose: print '\n----- begin optimizations -----'
-    odriver = opt_driver.OptDriver(specs_map, cline_opts, language=language)
-    optimized_code_seq = odriver.optimizeCodeFrags(cfrags, {}, True)
-    if verbose: print '----- finish optimizations -----'
+    # if orio was invoked as a compiler wrapper, perform the original command
+    if cline_opts.external_command:
+        cmd = ' '.join(cline_opts.external_command)
+        if verbose: print '----- invoking external (wrapped) command: -----' 
+        print cmd
+        os.system(cmd)
 
-    # remove all annotations from output
-    if cline_opts.erase_annot:
-        if verbose: print '\n----- begin removing annotations from output-----'
-        optimized_code_seq = [[ann_parser.AnnParser().removeAnns(c), i] \
-                              for c, i in optimized_code_seq]
-        if verbose: print '----- finish removing annotations from output-----'
+    if not cline_opts.disable_orio and cline_opts.rename_objects:
+        for srcfile, genfile in cline_opts.src_filenames.items():
+            genparts = genfile.split('.')
+            genobjfile = '.'.join(genparts[:-1])  + '.o'    # the Orio-generated object
+            srcparts = srcfile.split('.')
+            objfile = '.'.join(srcparts[:-1]) + '.o'     # the object corresponding to the input filename
+            if os.path.exists(genobjfile): os.system('mv %s %s' % (genobjfile,objfile))
 
-    # write output
-    if verbose: print '\n----- begin writing the output file(s) -----'
-    for optimized_code, input_params in optimized_code_seq:
-        out_filename = cline_opts.out_filename
-        if len(optimized_code_seq) > 1:
-            path_name, ext = os.path.splitext(cline_opts.out_filename)
-            suffix = ''
-            for pname, pval in input_params:
-                suffix += '_%s_%s' % (pname, pval)
-            out_filename = ('%s%s' % (path_name, suffix)) + ext
-        if verbose: print '--> writing output to: %s' % out_filename
-        try:
-            f = open(out_filename, 'w')
-            f.write(optimized_code)
-            f.close()
-        except:
-            print 'error: cannot open file for writing: %s' % cline_opts.out_filename
-            sys.exit(1)
-    if verbose: print '----- finish writing the output file(s) -----'
 
-    if verbose: print '\n====== END ORIO ======'
+
