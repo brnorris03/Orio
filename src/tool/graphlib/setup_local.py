12cb1138cad7db2244f502a9f4dbba6c52cf8655
Boyana Norris <brnorris03@gmail.com>
2010-10-19 18:58:32 +0000

    graph library
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@696 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

---
+++ src/tool/graphlib/setup_local.py
@@ -0,0 +1,15 @@
+import graph as m
+
+classifiers=[
+	'Development Status :: 4 - Beta',
+	'Intended Audience :: Science/Research',
+	'Intended Audience :: Developers',
+	'Programming Language :: Python',
+	'License :: OSI Approved :: Python Software Foundation License',
+	'Operating System :: OS Independent',
+	'Topic :: Software Development',
+	'Topic :: Scientific/Engineering'
+]
+
+kwargs = {}
+
