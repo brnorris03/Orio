2b9f8505197ad42d1084d3252a326d07af9374d8
pbalapra <pbalapra@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2011-03-22 18:41:11 +0000

    sc2011 paper

--- testsuite.v.01/NorHor.v.02.01/dgemv3/bto/dgemv3.ancc.c
+++ testsuite.v.01/NorHor.v.02.01/dgemv3/bto/dgemv3.ancc.c
@@ -7,8 +7,6 @@
   def build
   {
     arg build_command = 'gcc -O3 -lm';
-    #arg build_command  = 'icc -fast -openmp -I/usr/local/icc/include -lm';
-    #arg libs = '-lm';
   }
    
   def performance_counter         
@@ -19,39 +17,62 @@
   
   def performance_params
   {
+
+    param U1ia[] = [1,4,8,16,32,64,128,256,512];
+    param U1ja[] = [1,4,8,16,32,64,128,256,512];
+    param U2ia[] = [1,4,8,16,32,64,128,256,512];
+    param U3ia[] = [1,4,8,16,32,64,128,256,512];
+    param U3ja[] = [1,4,8,16,32,64,128,256,512];
+    param U4ia[] = [1,4,8,16,32,64,128,256,512];
+    param U5ia[] = [1,4,8,16,32,64,128,256,512];
+    param U5ja[] = [1,4,8,16,32,64,128,256,512];
+    param U6ia[] = [1,4,8,16,32,64,128,256,512];
+    param U7ia[] = [1,4,8,16,32,64,128,256,512];
+    param U7ja[] = [1,4,8,16,32,64,128,256,512];
+    param U8ia[] = [1,4,8,16,32,64,128,256,512];
+    param U9ia[] = [1,4,8,16,32,64,128,256,512];
+    param U10ia[] = [1,4,8,16,32,64,128,256,512];
+
+
     param U1i[] = range(1,31);
     param U1j[] = range(1,31);
+    param U2i[] = range(1,31);
     param U3i[] = range(1,31);
     param U3j[] = range(1,31);
-    param U3ja[] = range(1,31);
+    param U4i[] = range(1,31);
+    param U5i[] = range(1,31);
+    param U5j[] = range(1,31);
     param U6i[] = range(1,31);
-    param U6j[] = range(1,31);
-    param U6ja[] = range(1,31);
-    param U1[] = range(1,31);
-    param U2[] = range(1,31);
-    param U4[] = range(1,31);
-    param U5[] = range(1,31);
-    param U7[] = range(1,31);
-    #param U7[] = [1]; 
-    param U8[] = range(1,31);
-    param U9[] = range(1,31);
+    param U7i[] = range(1,31);
+    param U7j[] = range(1,31);
+    param U8i[] = range(1,31);
+    param U9i[] = range(1,31);
+    param U10i[] = range(1,31);
+
+    param PAR1i[] = [True, False];
+    param PAR1j[] = [True, False];
+    param PAR2i[] = [True, False];
+    param PAR3i[] = [True, False];
+    param PAR3j[] = [True, False];
+    param PAR4i[] = [True, False];
+    param PAR5i[] = [True, False];
+    param PAR5j[] = [True, False];
+    param PAR6i[] = [True, False];
+    param PAR7i[] = [True, False];
+    param PAR7j[] = [True, False];
+    param PAR8i[] = [True, False];
+    param PAR9i[] = [True, False];
+    param PAR10i[] = [True, False];
 
     param SCR[] = [True, False];
     param VEC[] = [True, False];
-    param OMP[] = [True, False];
-    param PAR1i[] = [True, False];
-    param PAR1j[] = [True, False];
 
-    #constraint reg_capacity = (2*U_I*U_J + 2*U_I + 2*U_J <= 130);
-
-    #constraint copy_limitA = ((not ACOPY_A) or (ACOPY_A and 
-    #                          (T1_I if T1_I>1 else T2_I)*(T1_J if T1_J>1 else T2_J) <= 512*512));
   }
 
   def search
   {
     arg algorithm = 'Randomsearch';
-    arg total_runs = 5000;
+    arg total_runs = 10;
   }
   
   def input_params
@@ -80,64 +101,106 @@ int i, j;
 register int it, jt;
 
 /*@ begin Loop (
+  
   transform Composite (
     scalarreplace = (SCR, 'double'),
-    regtile = (['j'], [U1]),
+    regtile = (['i','j'],[U1ia,U1ja]),  
     vector = (VEC, ['ivdep', 'vector always'])
   )
-  transform UnrollJam(ufactor=U1j, parallelize=PAR1j)
+  transform UnrollJam(ufactor=U1i, parallelize=PAR1i)
     for (i = 0; i <= A_ncols-1; i++) 
-      transform UnrollJam(ufactor=U1i, parallelize=PAR1i)
+      transform UnrollJam(ufactor=U1j, parallelize=PAR1j)
         for (j = 0; j <= A_nrows-1; j++) 
             t2[j] = t2[j] + A[i][j]*x[i]; 
 
-  transform Unroll (ufactor=U2)
+  transform Composite (
+    regtile = (['i'], [U2ia]),
+    vector = (VEC, ['ivdep', 'vector always'])
+  )
+  transform UnrollJam(ufactor=U2i, parallelize=PAR2i)
     for (i = 0; i <= A_nrows-1; i++) 
         y[i] = t2[i]+y[i]; 
 
   transform Composite(
     scalarreplace = (SCR, 'double'),
-    regtile = (['j'], [U3ja]),
+    regtile = (['i','j'],[U3ia,U3ja]),  
     vector = (VEC, ['ivdep', 'vector always'])
   )
+  transform UnrollJam(ufactor=U3i, parallelize=PAR3i)
     for (i = 0; i <= A_ncols-1; i++) 
-        for (j = 0; j <= A_nrows-1; j++) 
+       transform UnrollJam(ufactor=U3j, parallelize=PAR3j)    
+       for (j = 0; j <= A_nrows-1; j++) 
             t6[j] = t6[j] + A[i][j]*w[i]; 
 
-  transform Unroll(ufactor=U4)
-    for (i = 0; i <= A_nrows-1; i++) 
+  transform Composite (
+    regtile = (['i'], [U4ia]),
+    vector = (VEC, ['ivdep', 'vector always'])
+  )
+  transform UnrollJam(ufactor=U4i, parallelize=PAR4i)
+  for (i = 0; i <= A_nrows-1; i++) 
         z[i] = t6[i]+z[i]; 
-    for (i = 0; i <= A_ncols-1; i++) 
-        for (j = 0; j <= A_nrows; j++) 
-            t10[j] = t10[j] + A[i][j]*e[i]; 
 
-  transform Unroll(ufactor=U5)
-    for (i = 0; i <= A_nrows-1; i++) 
-        p[i] = t10[i]+p[i]; 
 
   transform Composite(
     scalarreplace = (SCR, 'double'),
-    regtile = (['j'], [U6ja]),
+    regtile = (['i','j'],[U5ia,U5ja]),     
     vector = (VEC, ['ivdep', 'vector always'])
   )
-	for (i = 0; i <= A_ncols-1; i++) 
-		for (j = 0; j <= A_nrows-1; j++) {
+  transform UnrollJam(ufactor=U5i, parallelize=PAR5i)
+  for (i = 0; i <= A_ncols-1; i++) 
+        transform UnrollJam(ufactor=U5j, parallelize=PAR5j)
+        for (j = 0; j <= A_nrows; j++) 
+            t10[j] = t10[j] + A[i][j]*e[i]; 
+
+
+  transform Composite (
+    regtile = (['i'], [U6ia]),
+    vector = (VEC, ['ivdep', 'vector always'])
+  )
+  transform UnrollJam(ufactor=U6i, parallelize=PAR6i)
+  for (i = 0; i <= A_nrows-1; i++) 
+    p[i] = t10[i]+p[i]; 
+
+
+  transform Composite(
+    scalarreplace = (SCR, 'double'),
+    regtile = (['i','j'],[U7ia,U7ja]),     
+    vector = (VEC, ['ivdep', 'vector always'])
+  )
+  transform UnrollJam(ufactor=U7i, parallelize=PAR7i)
+  for (i = 0; i <= A_ncols-1; i++) 
+    transform UnrollJam(ufactor=U7j, parallelize=PAR7j)
+    for (j = 0; j <= A_nrows-1; j++) {
 			t10[j] = t10[j] + e[i]*A[i][j]; 
 			t6[j] = t6[j] + w[i]*A[i][j]; 
 			t2[j] = t2[j] + A[i][j]*x[i]; 
 		}
-	
-  transform UnrollJam(ufactor=U7)
+
+  transform Composite (
+    regtile = (['i'], [U8ia]),
+    vector = (VEC, ['ivdep', 'vector always'])
+  )
+  transform UnrollJam(ufactor=U8i, parallelize=PAR8i)
 	for (i = 0; i <= A_nrows-1; i++) 
 		y[i] = t2[i]+y[i]; 
 
-  transform UnrollJam(ufactor=U8)
+  transform Composite (
+    regtile = (['i'], [U9ia]),
+    vector = (VEC, ['ivdep', 'vector always'])
+  )
+  transform UnrollJam(ufactor=U9i, parallelize=PAR9i)
 	for (i = 0; i <= A_nrows-1; i++) 
 		z[i] = t6[i]+z[i]; 
 
-  transform UnrollJam(ufactor=U9)
+
+  transform Composite (
+    regtile = (['i'], [U10ia]),
+    vector = (VEC, ['ivdep', 'vector always'])
+  )
+  transform UnrollJam(ufactor=U10i, parallelize=PAR10i)
 	for (i = 0; i <= A_nrows-1; i++) 
 		p[i] = t10[i]+p[i]; 
+
 ) @*/
 
 /* Noop */
