a92cd906e485be294c0160b77cabb88db73e19ed
pbalapra <pbalapra@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2011-03-23 02:46:35 +0000

    gemver
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@785 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- testsuite.v.01/NorHor.v.02.01/gemver/cookie/gemver.src2.c
+++ testsuite.v.01/NorHor.v.02.01/gemver/cookie/gemver.src2.c
@@ -11,30 +11,44 @@
   
   def performance_params 
   {
-  param U1[]  = range(1,31);
+
+  param U1i[]  = range(1,31);
   param U2i[] = [1]; # MUST be 1, otherwise it generates INCORRECT results (need debugging later)
   param U2j[] = range(1,31);
-  param U3[]  = range(1,31);
+  param U3i[]  = range(1,31);
   param U4i[] = range(1,31);
   param U4j[] = range(1,31);
 
-  param PAR1[] = [False,True];
-  param PAR2[] = [False,True]; 
-  param PAR3[] = [False,True];
-  param PAR4[] = [False,True];
 
-  param SCR1[] = [False,True]; 
+
+  param U2ia[] = [1,4,8,16,32];
+  param U2ja[] = [1,4,8,16,32];
+  param U3ia[]  = [1,4,8,16,32];
+  param U4ia[] = [1,4,8,16,32];
+  param U4ja[] = [1,4,8,16,32];
+
+  param PAR1i[] = [False,True];
+  param PAR2i[] = [False,True]; 
+  param PAR2j[] = [False,True]; 
+  param PAR3i[] = [False,True];
+  param PAR4i[] = [False,True];
+  param PAR4j[] = [False,True];
+
   param SCR2[] = [False,True];
+  param SCR3[] = [False,True];
+  param SCR4[] = [False,True];
 
   param VEC1[] = [False,True]; 
   param VEC2[] = [False,True];
   param VEC3[] = [False,True];
+  param VEC4[] = [False,True];
+
   }
 			 
   def search 
   { 
     arg algorithm = 'Randomsearch'; 
-    arg total_runs = 5000;
+    arg total_runs = 10;
   } 
   
   def input_params 
@@ -56,19 +70,21 @@ int i,j,ii,jj,it,jt;
 
 /*@ begin Loop(
 
-transform UnrollJam(ufactor=U1, parallelize=PAR1)
+transform UnrollJam(ufactor=U1i, parallelize=PAR1i)
 for (i=0;i<=n-1;i++) {
   x[i]=0;
   w[i]=0;
  }
 
+
 transform Composite(
- scalarreplace = (SCR1, 'double'),
- vector = (VEC1, ['ivdep','vector always'])
+ scalarreplace = (SCR2, 'double'),
+ regtile = (['j','i'],[U2ia,U2ja]),
+ vector = (VEC2, ['ivdep','vector always'])
 )
-transform UnrollJam(ufactor=U2i, parallelize=PAR2)
+transform UnrollJam(ufactor=U2i, parallelize=PAR2j)
 for (j=0;j<=n-1;j++) {
-  transform UnrollJam(ufactor=U2j)
+  transform UnrollJam(ufactor=U2j, parallelize=PAR2i)
   for (i=0;i<=n-1;i++) {
     B[j*n+i]=u2[j]*v2[i]+u1[j]*v1[i]+A[j*n+i];
     x[i]=y[j]*B[j*n+i]+x[i];
@@ -76,20 +92,24 @@ for (j=0;j<=n-1;j++) {
  }
 
 transform Composite(
- vector = (VEC2, ['ivdep','vector always'])
+ scalarreplace = (SCR3, 'double'),
+ regtile = (['i'],[U3ia]),
+ vector = (VEC3, ['ivdep','vector always'])
 )
-transform UnrollJam(ufactor=U3, parallelize=PAR3)
+transform UnrollJam(ufactor=U3i, parallelize=PAR3i)
 for (i=0;i<=n-1;i++) {
   x[i]=b*x[i]+z[i];
  }
 
+
 transform Composite(
- scalarreplace = (SCR2, 'double'),
- vector = (VEC3, ['ivdep','vector always'])
+ scalarreplace = (SCR4, 'double'),
+ regtile = (['i','j'],[U4ia,U4ja]),
+ vector = (VEC4, ['ivdep','vector always'])
 )
-transform UnrollJam(ufactor=U4i, parallelize=PAR4)
+transform UnrollJam(ufactor=U4i, parallelize=PAR4i)
 for (i = 0; i <= n-1; i++) {
-  transform UnrollJam(ufactor=U4j)
+  transform UnrollJam(ufactor=U4j,parallelize=PAR4j)
   for (j = 0; j <= n-1; j++) {
     w[i] = w[i] + B[i*n+j]*x[j];
   }
