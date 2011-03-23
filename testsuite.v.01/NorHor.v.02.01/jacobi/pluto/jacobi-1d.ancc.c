99990e7b50919fad5e22df6a59f2b92f59736b12
pbalapra <pbalapra@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2011-03-23 05:00:34 +0000

    update

--- testsuite.v.01/NorHor.v.02.01/jacobi/pluto/jacobi-1d.ancc.c
+++ testsuite.v.01/NorHor.v.02.01/jacobi/pluto/jacobi-1d.ancc.c
@@ -1,45 +1,26 @@
 /*@ begin PerfTuning (          
   def build  
   {  
-    arg command = 'icc';  
-    arg options = '-fast -openmp -I/usr/local/icc/include -lm';  
+  arg build_command = 'icc -O3 -openmp -lm -DDYNAMIC'; 
   }  
      
   def performance_counter           
   {  
-    arg method = 'basic timer';  
-    arg repetitions = 1;  
+    arg repetitions = 35;  
   } 
  
   def performance_params  
   { 
-#    param T1_1[] = [1,16,32,64,128,256];
-#    param T1_2[] = [1,16,32,64,128,256];
-#    param T2_1[] = [1,2,4,8,16,32];
-#    param T2_2[] = [1,2,4,8,16,32];
+    param U1ta[] = [1,16,32,64,128,256];
+    param U1ia[] = [1,16,32,64,128,256];
+    
+    param U1t[] = range(1,31);
+    param U1i[] = range(1,31);
 
-    param T1_1[] = [32]; 
-    param T1_2[] = [128];
-    param T2_1[] = [1]; 
-    param T2_2[] = [8]; 
- 
-#    constraint tile_size1 = (T1_1*T2_1) <= 513;
-#    constraint tile_size2 = (T1_2*T2_2) <= 513;
-
-#    param U1[] = [1,2,4,6,8];
-#    param U2[] = [1,2,4,6,8];
-
-    param U1[] = [2];
-    param U2[] = [8];
-
-    param PERM[] = [
-     [0,1],
-#     [1,0],
-    ];
-
-    param PAR[] = [True]; 
-    param SCREP[] = [False]; 
-    param IVEC[] = [True]; 
+    param PAR1t[] = [False,True]; 
+    param PAR1i[] = [False,True]; 
+    param SCR[] = [False,True]; 
+    param VEC[] = [False,True]; 
   } 
  
   def input_params
@@ -54,17 +35,10 @@
    arg init_file = 'jacobi-1d_init_code.c';
   } 
 
-  def performance_test_code 
-  {
-    arg skeleton_code_file = 'jacobi-1d_skeleton_code.c';
-  }
-
   def search  
   {
-    arg algorithm = 'Exhaustive';  
-#    arg algorithm = 'Simplex';  
-#    arg time_limit = 1;
-#    arg total_runs = 1; 
+    arg algorithm = 'Randomsearch';  
+    arg total_runs = 20; 
   }
 ) @*/  
 
@@ -76,25 +50,19 @@ register int newlb_c1, newlb_c2, newlb_c3, newlb_c4, newlb_c5, newlb_c6,
 register int newub_c1, newub_c2, newub_c3, newub_c4, newub_c5, newub_c6, 
   newub_c7, newub_c8, newub_c9, newub_c10, newub_c11, newub_c12; 
 
-/*@ begin PolySyn( 
-  parallel = PAR;
-  tiles = [T1_1,T1_2,T2_1,T2_2];
-  permut = PERM;
-  unroll_factors = [U1,U2];
-  scalar_replace = SCREP;
-  vectorize = IVEC;
- 
-  profiling_code = 'jacobi-1d_profiling.c'; 
-  compile_cmd = 'gcc'; 
-  compile_opts = '-lm'; 
-) @*/ 
-
-/* pluto start (T,N) */
+/*@ begin Loop(
+transform Composite(
+scalarreplace = (SCR, 'double'),
+regtile = (['t','i'],[U1ta,U1ia]),
+vector = (VEC, ['ivdep','vector always'])
+)
+transform UnrollJam(ufactor=U1t, parallelize=PAR1t)
 for (t=1; t<=T-1; t++) 
+  transform UnrollJam(ufactor=U1i, parallelize=PAR1i)
   for (i=1; i<=N-2; i++) 
     a[t][i] = 0.333 * (a[t-1][i-1] + a[t-1][i] + a[t-1][i+1]);
-/* pluto end */
 
+) @*/
 /*@ end @*/
 /*@ end @*/
 
