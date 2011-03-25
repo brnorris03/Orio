34a501dbd74d01d776b8e62c128cf1206b690589
norris <norris@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2011-03-25 00:05:38 +0000

    major changes to the tuning spec (tried to make as complete as possible).
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@795 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- testsuite/atax/cookie/atax2.src2.c
+++ testsuite/atax/cookie/atax2.src2.c
@@ -1,7 +1,8 @@
 /*@ begin PerfTuning (  
   def build
   {
-  arg build_command = 'icc -O3 -openmp -lm -DDYNAMIC'; 
+  arg build_command = 'icc -O3 -openmp -DDYNAMIC'; 
+  arg libs = '-lm -lrt';
   }
   
   def performance_counter  
@@ -11,16 +12,47 @@
   
   def performance_params 
   {
-  param U1[]   = [4];
-  param U2i[]  = [22,24,26,28,30];
-  param U2ja[] = [1];
-  param U2jb[] = [1];
+    # Cache tiling
+    param T1_I[] = [1,16,32,64,128,256,512];
+    param T1_J[] = [1,16,32,64,128,256,512];
+    param T1_K[] = [1,16,32,64,128,256,512];
+    param T2_I[] = [1,64,128,256,512,1024,2048];
+    param T2_J[] = [1,64,128,256,512,1024,2048];
+    param T2_K[] = [1,64,128,256,512,1024,2048];
 
-  param PAR1[] = [False];
-  param PAR2[] = [True];
-  param SCR[]  = [True];
-  param VEC1[] = [True];
-  param VEC2[] = [True];
+    # Array copy
+    param ACOPY_x[] = [False,True];
+    param ACOPY_y[] = [False,True];
+
+    # Unroll-jam 
+    param U1_I[] = [1,4,8,12,16];
+    param U_I[] = [1,4,8];
+    param U_J[] = [1,4,8];
+    param U_K[] = [1,4,8];
+
+    # Register tiling
+    param RT_I[] = [1,8,32];
+    param RT_J[] = [1,8,32];
+    param RT_K[] = [1,8,32];
+
+    # Scalar replacement
+    param SCR[]  = [False,True];
+
+    # Vectorization
+    param VEC1[] = [False,True];
+    param VEC2[] = [False,True];
+
+    # Parallelization
+    param OMP[] = [True,False];
+
+    # Constraints
+    constraint tileI = ((T2_I == 1) or (T2_I % T1_I == 0));
+    constraint tileJ = ((T2_J == 1) or (T2_J % T1_J == 0));
+    constraint tileK = ((T2_K == 1) or (T2_K % T1_K == 0));
+
+    constraint reg_capacity = (RT_I*RT_J + RT_I*RT_K + RT_J*RT_K <= 150);
+    constraint unroll_limit = ((U_I == 1) or (U_J == 1) or (U_K == 1));
+
   }
 			 
   def search 
@@ -45,32 +77,39 @@
 #define max(x,y)    ((x) > (y)? (x) : (y))
 #define min(x,y)    ((x) < (y)? (x) : (y))
 
-int i,j;
+int i,j, k;
+int it, jt, kt;
+int ii, jj, kk;
+int iii, jjj, kkk;
+
 double* tmp=(double*) malloc(nx*sizeof(double));
   
 /*@ begin Loop(
 
   transform Composite(
+    unrolljam = (['i'],[U1_I]),
     vector = (VEC1, ['ivdep','vector always'])
   )
-  transform UnrollJam(ufactor=U1, parallelize=PAR1)
   for (i= 0; i<=ny-1; i++)
     y[i] = 0.0;
 
-
   transform Composite(
+    tile = [('i',T1_I,'ii'),('j',T1_J,'jj'),('k',T1_K,'kk'),
+            (('ii','i'),T2_I,'iii'),(('jj','j'),T2_J,'jjj'),(('kk','k'),T2_K,'kkk')],
+    arrcopy = [(ACOPY_y, 'y[k]', [(T1_K if T1_K>1 else T2_K)],'_copy'),
+               (ACOPY_x, 'x[j]', [(T1_J if T1_J>1 else T2_J)],'_copy')],
+    unrolljam = (['k','j','i'],[U_K,U_J,U_I]),
     scalarreplace = (SCR, 'double'),
-    vector = (VEC2, ['ivdep','vector always'])
+    regtile = (['i','j','k'],[RT_I,RT_J,RT_K]),
+    vector = (VEC2, ['ivdep','vector always']),
+    openmp = (OMP, 'omp parallel for private(iii,jjj,kkk,ii,jj,kk,i,j,k,y_copy,x_copy)')
   )
-  transform UnrollJam(ufactor=U2i, parallelize=PAR2)
   for (i = 0; i<=nx-1; i++) {
     tmp[i] = 0;
-    transform UnrollJam(ufactor=U2ja)
     for (j = 0; j<=ny-1; j++) 
       tmp[i] = tmp[i] + A[i*ny+j]*x[j];
-    transform UnrollJam(ufactor=U2jb)
-    for (j = 0; j<=ny-1; j++) 
-      y[j] = y[j] + A[i*ny+j]*tmp[i];
+    for (k = 0; k<=ny-1; k++) 
+      y[k] = y[k] + A[i*ny+k]*tmp[i];
   }
 ) @*/
 /*@ end @*/
