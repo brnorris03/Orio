329c0a96187d3047435fb5a5ceb367128101e1a8
Azamat Mametjanov <azamat@mcs.anl.gov>
2012-02-23 19:13:32 +0000

    cuda kernel AXPBYPCZ
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@954 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

---
+++ testsuite/sandbox/cuda/vecAXPBYPCZ.c
@@ -0,0 +1,45 @@
+void VecAXPBYPCZ(int n, double a, double *x, double b, double *y, double c, double *z) {
+
+    register int i;
+
+    /*@ begin PerfTuning (
+          def performance_params {
+            param TC[] = range(16,17,16);
+            param CB[] = [True, False];
+            param PHM[] = [False];
+            param SC[] = range(1,3);
+          }
+          def build {
+            arg build_command = 'nvcc -arch=sm_20';
+          }
+          def input_params {
+            param N[] = [1000];
+          }
+          def input_vars {
+            decl double a = random;
+            decl double b = random;
+            decl double c = random;
+            decl static double x[N] = random;
+            decl static double y[N] = random;
+            decl static double z[N] = random;
+          }
+          def performance_counter {
+            arg method = 'basic timer';
+            arg repetitions = 10;
+          }
+    ) @*/
+
+    int n=N;
+
+    /*@ begin Loop (
+          transform CUDA(threadCount=TC, cacheBlocks=CB, pinHostMem=PHM, streamCount=SC)
+        for (i=0; i<=n-1; i++)
+          y[i]=a*x[i]+b*y[i]+c*z[i];
+    ) @*/
+
+    for (i=0; i<=n-1; i++)
+        y[i]=a*x[i]+b*y[i];
+
+    /*@ end @*/
+    /*@ end @*/
+}
