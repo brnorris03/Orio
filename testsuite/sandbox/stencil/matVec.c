229339ef9ebba50d108a118e9fd37fbdcc84b1e2
azamat <azamat@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2012-03-02 04:26:32 +0000

    stencil syntax

---
+++ testsuite/sandbox/stencil/matVec.c
@@ -0,0 +1,19 @@
+void MatVec_Stencil(int m, int n, double** A, double* x, double* y) {
+  /*@
+    begin Stencil (
+      transform Domain(
+        gridDim = 3,
+        gridSize = [5, 5, 5],
+        stencilType = star,
+        stencilWidth = 1,
+        dof = 1,
+        targetPlatform = GPU
+      )
+      y = A * x
+    )
+  @*/
+
+  // placeholder
+
+  /*@ end @*/
+}
