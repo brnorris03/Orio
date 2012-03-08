1d78ab2e2f5a57ce6d346a8bc63fb8788153131b
Azamat Mametjanov <azamat@mcs.anl.gov>
2012-03-08 23:20:04 +0000

    cuda stencil mv impl
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@974 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- testsuite/sandbox/cuda/matVec.c
+++ testsuite/sandbox/cuda/matVec.c
@@ -1,14 +1,40 @@
 void MatVec_StencilSG(int n, double* A, double* x, double* y) {
 
+  /*@ begin PerfTuning (
+        def performance_params {
+          param TC[] = range(16,17,16);
+          param CB[] = [False];
+          param PHM[] = [False];
+          param SC[] = range(1,2);
+        }
+        def build {
+          arg build_command = 'nvcc -arch=sm_20';
+        }
+        def input_params {
+          param N[] = [125];
+        }
+        def input_vars {
+          decl static double A[N] = random;
+          decl static double x[N] = random;
+          decl static double y[N] = 0;
+        }
+        def performance_counter {
+          arg method = 'basic timer';
+          arg repetitions = 10;
+        }
+  ) @*/
+
   register int s;
+  int n=N;
 
   /*@
     begin Loop(
-      transform CUDA(threadCount=16, cacheBlocks=False, pinHostMem=False, streamCount=1, domain='Stencil_SG3_Star1_Dof1')
+      transform CUDA(threadCount=TC, cacheBlocks=CB, pinHostMem=PHM, streamCount=SC, domain='Stencil_SG3_Star1_Dof1')
         for(s=0; s<=n-1; s++)
-          y[s] = A[s] * x[s];
+          y[s] += A[s] * x[s];
     )
   @*/
 
+  /*@ end @*/
   /*@ end @*/
 }
