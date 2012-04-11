3bffaba00dacd760badbda9fbc8e96dfc714bc0c
Azamat Mametjanov <azamat@mcs.anl.gov>
2012-04-11 04:27:49 +0000

    dia formfunction2d
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@1001 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

---
+++ testsuite/sandbox/cuda/bratu2D.c
@@ -0,0 +1,54 @@
+void FormFunction2D(double lambda, int m, int n, double* X, double *F) {
+  /*@ begin PerfTuning (
+        def performance_params {
+          param TC[]  = range(32,33,32);
+          param CB[]  = [True];
+          param PHM[] = [False];
+        }
+        def input_params {
+          param m   = 512;
+          param n   = 512;
+          param Nos = 5;
+          param lambda = 6;
+        }
+        def input_vars {
+          decl static double X[m*n*Nos] = random;
+          decl static double F[m*n*Nos] = 0;
+        }
+        def build {
+          arg build_command = 'nvcc -arch=sm_20';
+        }
+        def performance_counter {
+          arg method = 'basic timer';
+          arg repetitions = 10;
+        }
+  ) @*/
+
+  int i;
+  int nrows=m*n;
+  int offsets[Nos];
+  offsets[0]=-m;
+  offsets[1]=-1;
+  offsets[2]=0;
+  offsets[3]=1;
+  offsets[4]=m;
+  int bb = offsets[0];
+  int be = offsets[4];
+
+  double hx     = 1.0/(m-1);
+  double hy     = 1.0/(n-1);
+  double sc     = hx*hy*lambda;
+  double hxdhy  = hx/hy;
+  double hydhx  = hy/hx;
+
+  /*@ begin Loop(transform CUDA(threadCount=TC, cacheBlocks=CB, pinHostMem=PHM)
+  for(i=bb; i<=be-1; i++) {
+    F[i] = (2*X[i+2*nrows] - X[i+nrows] - X[i+3*nrows])*hydhx + (2*X[i+2*nrows] - X[i] - X[i+4*nrows])*hxdhy - sc*exp(X[i+2*nrows]);
+  }
+  ) @*/
+  for(i=bb; i<=be-1; i++) {
+    F[i] = (2*X[i+2*nrows] - X[i+nrows] - X[i+3*nrows])*hydhx + (2*X[i+2*nrows] - X[i] - X[i+4*nrows])*hxdhy - sc*exp(X[i+2*nrows]);
+  }
+  /*@ end @*/
+  /*@ end @*/
+}
