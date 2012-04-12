aa81eccb339264b207b2d7599dcae6a1379f2bfb
azamat <azamat@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2012-04-12 16:26:55 +0000

    utest

---
+++ testsuite/regression/cuda/sources/bratu2D.c
@@ -0,0 +1,29 @@
+void FormFunction2D(double lambda, int m, int n, double* X, double *F) {
+  int i;
+  int nrows=m*n;
+  int offsets[Nos];
+  offsets[0]=-m;
+  offsets[1]=-1;
+  offsets[2]=0;
+  offsets[3]=1;
+  offsets[4]=m;
+  int bb = offsets[4];
+  int be = nrows-offsets[4];
+
+  double hx     = 1.0/(m-1);
+  double hy     = 1.0/(n-1);
+  double sc     = hx*hy*lambda;
+  double hxdhy  = hx/hy;
+  double hydhx  = hy/hx;
+  double u;
+
+  /*@ begin Loop(transform CUDA(threadCount=32, cacheBlocks=False, pinHostMem=False)
+  for(i=bb; i<=be-1; i++) {
+    F[i] = (2*X[i+2*nrows] - X[i+nrows] - X[i+3*nrows])*hydhx + (2*X[i+2*nrows] - X[i] - X[i+4*nrows])*hxdhy - sc*exp(X[i+2*nrows]);
+  }
+  ) @*/
+  for(i=bb; i<=be-1; i++) {
+    F[i] = (2*X[i+2*nrows] - X[i+nrows] - X[i+3*nrows])*hydhx + (2*X[i+2*nrows] - X[i] - X[i+4*nrows])*hxdhy - sc*exp(X[i+2*nrows]);
+  }
+  /*@ end @*/
+}
