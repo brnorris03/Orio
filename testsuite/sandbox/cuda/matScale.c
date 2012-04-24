03dc626273b4f9ef065f8fea2f172fb3db0d9812
azamat <azamat@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2012-04-24 15:49:14 +0000

    MatScale
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@1022 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

---
+++ testsuite/sandbox/cuda/matScale.c
@@ -0,0 +1,39 @@
+void MatScale_SeqDIA(double* A, double a) {
+
+  /*@ begin PerfTuning (
+        def performance_params {
+          param TC[]  = range(16,33,16);
+          param CB[]  = [True, False];
+          param PHM[] = [False];
+          param SC[]  = range(1,3);
+        }
+        def input_params {
+          param N[] = [1000];
+        }
+        def input_vars {
+          decl dynamic double A[N] = random;
+          decl double a = random;
+        }
+        def build {
+          arg build_command = 'nvcc -arch=sm_20';
+        }
+        def performance_counter {
+          arg method = 'basic timer';
+          arg repetitions = 10;
+        }
+  ) @*/
+
+  register int i;
+  int nz = N;
+
+  /*@ begin Loop(transform CUDA(threadCount=TC, cacheBlocks=CB, pinHostMem=PHM, streamCount=SC)
+        for(i=0;i<=nz-1;i++) {
+          A[nz] *= a;
+        }
+  ) @*/
+  for(i=0;i<=nz-1;i++) {
+    A[nz] *= a;
+  }
+  /*@ end @*/
+  /*@ end @*/
+}
