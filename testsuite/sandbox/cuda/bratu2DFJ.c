38ad7b5c175793c2f8140d1e13037b64548844b3
azamat <azamat@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2012-05-01 19:22:10 +0000

    FormJacobian2D kernel

---
+++ testsuite/sandbox/cuda/bratu2DFJ.c
@@ -0,0 +1,62 @@
+void FormJacobian2D(double lambda, int m, int n, double *x, double *dia) {
+  int i;
+  /*@ begin PerfTuning (
+        def performance_params {
+          param TC[] = range(32,65,32);
+          param CB[] = [True, False];
+          param SC[] = range(1,3);
+        }
+        def input_params {
+          param Lambda = 6;
+          param M = 4;
+          param N = 4;
+        }
+        def input_vars {
+          decl dynamic double x[M*N] = random;
+          decl dynamic double dia[5*M*N-2*M-2] = 0;
+        }
+        def build {
+          arg build_command = 'nvcc -arch=sm_20';
+        }
+        def performance_counter {
+          arg method = 'basic timer';
+          arg repetitions = 1;
+        }
+  ) @*/
+
+  int m = M;
+  int n = N;
+  double lambda = Lambda;
+  double hx     = 1.0/(m-1);
+  double hy     = 1.0/(n-1);
+  double sc     = hx*hy*lambda;
+  double hxdhy  = hx/hy;
+  double hydhx  = hy/hx;
+
+  int nrows=m*n;
+  int bb = m;
+  int be = nrows-m;
+
+  /*@ begin Loop(transform CUDA(threadCount=TC, cacheBlocks=CB, streamCount=SC)
+
+  for(i=bb; i<=be-1; i++) {
+    dia[i-m]           = -hxdhy;
+    dia[i+nrows-m-1]   = -hydhx;
+    dia[i+2*nrows-m-1] = 2.0*(hydhx + hxdhy) - sc*exp(x[i]);
+    dia[i+3*nrows-m-1] = -hydhx;
+    dia[i+5*nrows-m-2] = -hxdhy;
+  }
+
+  ) @*/
+
+  for(i=bb; i<=be-1; i++) {
+    dia[i-m]           = -hxdhy;
+    dia[i+nrows-m-1]   = -hydhx;
+    dia[i+2*nrows-m-1] = 2.0*(hydhx + hxdhy) - sc*exp(x[i]);
+    dia[i+3*nrows-m-1] = -hydhx;
+    dia[i+5*nrows-m-2] = -hxdhy;
+  }
+
+  /*@ end @*/
+  /*@ end @*/
+}
