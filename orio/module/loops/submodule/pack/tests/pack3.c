82f307710aedc9286cb485c36f0511551940f509
Azamat Mametjanov <azamat@mcs.anl.gov>
2012-09-06 23:38:35 +0000

    pointer test
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@1213 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

---
+++ orio/module/loops/submodule/pack/tests/pack3.c
@@ -0,0 +1,47 @@
+void PackAligned(double* src, double* dest, const int stride, const int nelms, const int cnt) {
+
+  /*@ begin PerfTuning (
+        def performance_params {
+          param PTRS[] = [('src')];
+          param CFLAGS[] = map(join, product(['-fprefetch-loop-arrays']));
+        }
+        def input_params {
+          param nelms[] = range(2,3);
+          param cnt[] = range(3,4);
+          param stride[] = range(4,5);
+        }
+        def input_vars {
+          decl dynamic double  src[nelms*cnt*stride] = random;
+          decl dynamic double dest[nelms*cnt] = 0;
+        }
+        def build {
+          arg build_command = 'gcc @CFLAGS';
+        }
+        def performance_counter {
+          arg method = 'basic timer';
+          arg repetitions = 6;
+        }
+  ) @*/
+
+  register int i, j;
+  double *init_dest=dest, *init_src=src;
+
+  /*@ begin Loops(transform Pack(prefetch=PTRS)
+
+  for(i=cnt; i; i--) {
+    for(j=nelms; j; j--) {
+      *dest++ = *src++;
+    }
+    src += stride-nelms;
+  }
+
+  ) @*/
+
+
+  /*@ end @*/
+
+  dest=init_dest;
+  src=init_src;
+
+  /*@ end @*/
+}
