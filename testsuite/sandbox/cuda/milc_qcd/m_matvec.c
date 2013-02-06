6ddc4cf15b3a49f2b3e960b2f1c23ab12de88376
azamat <azamat@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2013-02-06 18:55:48 +0000

    version with all nested loops
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@1271 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- testsuite/sandbox/cuda/milc_qcd/m_matvec.c
+++ testsuite/sandbox/cuda/milc_qcd/m_matvec.c
@@ -5,33 +5,24 @@
 */
 void mult_su3_mat_vec(su3_matrix **A, su3_vector **x, su3_vector **y ){
   const int sites_on_node = 10; // or some other global constant value
-  register int i,j;
+  register int i,j,k;
   register double ar,ai,br,bi,cr,ci;
 
   for(i=0; i<sites_on_node; i++) {
 
     for(j=0; j<3; j++) {
 
-      ar=A[i]->e[j][0].real;
-      ai=A[i]->e[j][0].imag;
-      br=x[i]->c[0].real;
-      bi=x[i]->c[0].imag;
-      cr  = ar*br - ai*bi;
-      ci  = ar*bi + ai*br;
+      cr = ci = 0.0;
 
-      ar=A[i]->e[j][1].real;
-      ai=A[i]->e[j][1].imag;
-      br=x[i]->c[1].real;
-      bi=x[i]->c[1].imag;
-      cr += ar*br - ai*bi;
-      ci += ar*bi + ai*br;
+      for(k=0; k<3; k++) {
 
-      ar=A[i]->e[j][2].real;
-      ai=A[i]->e[j][2].imag;
-      br=x[i]->c[2].real;
-      bi=x[i]->c[2].imag;
-      cr += ar*br - ai*bi;
-      ci += ar*bi + ai*br;
+        ar=A[i]->e[j][k].real;
+        ai=A[i]->e[j][k].imag;
+        br=x[i]->c[k].real;
+        bi=x[i]->c[k].imag;
+        cr += ar*br - ai*bi;
+        ci += ar*bi + ai*br;
+      }
 
       y[i]->c[j].real=cr;
       y[i]->c[j].imag=ci;
