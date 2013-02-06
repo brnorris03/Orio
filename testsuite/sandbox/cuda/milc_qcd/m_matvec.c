e84bf37d4bef96613d41dfa18a024963ffe88a85
azamat <azamat@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2013-02-06 19:51:07 +0000

    simplified version without pointers and structs

--- testsuite/sandbox/cuda/milc_qcd/m_matvec.c
+++ testsuite/sandbox/cuda/milc_qcd/m_matvec.c
@@ -3,29 +3,29 @@
 * matrix vector multiply                                                *
 *  y[i]  <-  A[i]*x[i]                                                  *
 */
-void mult_su3_mat_vec(su3_matrix **A, su3_vector **x, su3_vector **y ){
+void mult_su3_mat_vec(double A[], double x[], double y[]) {
   const int sites_on_node = 10; // or some other global constant value
   register int i,j,k;
   register double ar,ai,br,bi,cr,ci;
 
   for(i=0; i<sites_on_node; i++) {
 
-    for(j=0; j<3; j++) {
+    for(j=0; j<6; j+=2) {
 
       cr = ci = 0.0;
 
-      for(k=0; k<3; k++) {
+      for(k=0; k<6; k+=2) {
 
-        ar=A[i]->e[j][k].real;
-        ai=A[i]->e[j][k].imag;
-        br=x[i]->c[k].real;
-        bi=x[i]->c[k].imag;
+        ar=A[18*i+3*j+k];
+        ai=A[18*i+3*j+k+1];
+        br=x[6*i+k];
+        bi=x[6*i+k+1];
         cr += ar*br - ai*bi;
         ci += ar*bi + ai*br;
       }
 
-      y[i]->c[j].real=cr;
-      y[i]->c[j].imag=ci;
+      y[6*i+j]  =cr;
+      y[6*i+j+1]=ci;
     }
   }
 }
