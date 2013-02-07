014f51d653a3ff517862a2f6a3e6983d38b5c48c
Azamat Mametjanov <azamat@mcs.anl.gov>
2013-02-07 01:14:03 +0000

    orcuda tuneable 1dir matvec
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@1274 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- testsuite/sandbox/cuda/milc_qcd/m_matvec.c
+++ testsuite/sandbox/cuda/milc_qcd/m_matvec.c
@@ -8,14 +8,39 @@ void mult_su3_mat_vec(double A[], double x[], double y[]) {
   register int i,j,k;
   register double ar,ai,br,bi,cr,ci;
 
-  for(i=0; i<sites_on_node; i++) {
+  /*@ begin PerfTuning (
+        def performance_params {
+          param TC[]  = range(32,1025,32);
+          param BC[]  = range(14,113,14);
+          param UIF[] = range(1,6);
+          param PL[]  = [16,48];
+          param CFLAGS[] = map(join, product(['-O0', '-O1', '-O2', '-O3']));
+        }
+        def input_params {
+          param SITES[] = [2,4,6,8,10,12,14,16];
+        }
+        def input_vars {
+          decl dynamic double A[18*SITES] = random;
+          decl dynamic double x[6*SITES]  = random;
+          decl dynamic double y[6*SITES]  = 0;
+        }
+        def build {
+          arg build_command = 'nvcc -arch=sm_20 @CFLAGS';
+        }
+        def performance_counter {
+          arg method = 'basic timer';
+          arg repetitions = 6;
+        }
+  ) @*/
 
-    for(j=0; j<6; j+=2) {
+  int sites_on_node=SITES;
 
+  /*@ begin Loop(transform CUDA(threadCount=TC, blockCount=BC, preferL1Size=PL, unrollInner=UIF)
+
+  for(i=0; i<=sites_on_node-1; i++) {
+    for(j=0; j<=5; j+=2) {
       cr = ci = 0.0;
-
-      for(k=0; k<6; k+=2) {
-
+      for(k=0; k<=5; k+=2) {
         ar=A[18*i+3*j+k];
         ai=A[18*i+3*j+k+1];
         br=x[6*i+k];
@@ -23,9 +48,28 @@ void mult_su3_mat_vec(double A[], double x[], double y[]) {
         cr += ar*br - ai*bi;
         ci += ar*bi + ai*br;
       }
-
       y[6*i+j]  =cr;
       y[6*i+j+1]=ci;
     }
   }
+
+  ) @*/
+
+  for(i=0; i<=sites_on_node-1; i++) {
+    for(j=0; j<6; j+=2) {
+      cr = ci = 0.0;
+      for(k=0; k<6; k+=2) {
+        ar=A[18*i+3*j+k];
+        ai=A[18*i+3*j+k+1];
+        br=x[6*i+k];
+        bi=x[6*i+k+1];
+        cr += ar*br - ai*bi;
+        ci += ar*bi + ai*br;
+      }
+      y[6*i+j]  =cr;
+      y[6*i+j+1]=ci;
+    }
+  }
+  /*@ end @*/
+  /*@ end @*/
 }
