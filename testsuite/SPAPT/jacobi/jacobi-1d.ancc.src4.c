cac20dd46b123d74aeab665d4807cd013896195b
pbalapra <pbalapra@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2013-03-18 21:15:43 +0000

    sources

--- testsuite/SPAPT/jacobi/jacobi-1d.ancc.src4.c
+++ testsuite/SPAPT/jacobi/jacobi-1d.ancc.src4.c
@@ -18,16 +18,16 @@
     param T2_Ia[] = [1,64,128,256,512,1024,2048];
     param T2_Ja[] = [1,64,128,256,512,1024,2048];
 
-    param U2_I[]  = range(1,31); 
-    param U2_J[]  = range(1,31); 
+    param U_I[]  = range(1,31); 
+    param U_J[]  = range(1,31); 
 
-    param RT2_I[] = [1,8,32];
-    param RT2_J[] = [1,8,32];
+    param RT_I[] = [1,8,32];
+    param RT_J[] = [1,8,32];
 
-
-
-
-
+    constraint tileI2 = ((T2_Ia == 1) or (T2_Ia % T2_I == 0));
+    constraint tileJ2 = ((T2_Ja == 1) or (T2_Ja % T2_J == 0));
+    constraint reg_capacity = (RT_I*RT_J <= 150);
+    constraint unroll_limit = ((U_I == 1) or (U_J == 1));
 
   } 
  
@@ -46,8 +46,14 @@
   def search  
   {
     arg algorithm = 'Randomsearch';  
-    arg total_runs = 10000; 
+    arg total_runs = 100000; 
   }
+
+
+  def validation {
+      arg validation_file = 'validation.c';
+  }
+
 ) @*/  
 
 
@@ -62,8 +68,8 @@ int iii, jjj, kkk;
 transform Composite(
       tile = [('i',T2_I,'ii'),('j',T2_J,'jj'),
              (('ii','i'),T2_Ia,'iii'),(('jj','j'),T2_Ja,'jjj')],
-      unrolljam = (['i','j'],[U2_I,U2_J]),
-      regtile = (['i','j'],[RT2_I,RT2_J]),
+      unrolljam = (['i','j'],[U_I,U_J]),
+      regtile = (['i','j'],[RT_I,RT_J])
 )
 for (i=1; i<=T-1; i++) 
   for (j=1; j<=N-2; j++) 
