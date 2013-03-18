283541fb417dfc41bcebf5c1836df3d73b967bc9
pbalapra <pbalapra@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2013-03-18 21:16:04 +0000

    sources

--- testsuite/SPAPT/mvt/mvt.ancc.src4.c
+++ testsuite/SPAPT/mvt/mvt.ancc.src4.c
@@ -13,37 +13,36 @@
   
   def performance_params
   {
-    param T1_I[] = [1,16,32,64,128,256,512];
-    param T1_J[] = [1,16,32,64,128,256,512];
-    param T2_I[] = [1,64,128,256,512,1024,2048];
-    param T2_J[] = [1,64,128,256,512,1024,2048];
+
+    param T2_I[] = [1,16,32,64,128,256,512];
+    param T2_J[] = [1,16,32,64,128,256,512];
+    param T2_Ia[] = [1,64,128,256,512,1024,2048];
+    param T2_Ja[] = [1,64,128,256,512,1024,2048];
+
+    param U2_I[]  = range(1,31); 
+    param U2_J[]  = range(1,31); 
+
+    param RT2_I[] = [1,8,32];
+    param RT2_J[] = [1,8,32];
 
 
-    param U_I[] = range(1,31);
-    param U_J[] = range(1,31);
-
+    constraint tileI2 = ((T2_Ia == 1) or (T2_Ia % T2_I == 0));
+    constraint tileJ2 = ((T2_Ja == 1) or (T2_Ja % T2_J == 0));
+    constraint reg_capacity = (RT2_I*RT2_J <= 150);
+    constraint unroll_limit = ((U2_I == 1) or (U2_J == 1));
     
-    
-
-
-
-                              (T1_I if T1_I>1 else T2_I)*(T1_J if T1_J>1 else T2_J) <= 512*512));
-  }
+   }
 
   def search
   {
     arg algorithm = 'Randomsearch';
-    arg total_runs = 10000;
+    arg total_runs = 100000;
   }
-  
-  let SIZE = 2500;
   
   def input_params
   {
-    param MSIZE = SIZE;
-    param NSIZE = SIZE;
-    param M = SIZE;
-    param N = SIZE;
+    param M = 2500;
+    param N = 2500;
   }
 
   def input_vars
@@ -55,12 +54,17 @@
     decl static double x2[N] = 0;
   }
 
+  def validation {
+    arg validation_file = 'validation.c';
+  }
+
             
 ) @*/
 
 int i, j;
 int ii, jj;
 int iii, jjj;
+int it, jt;
 
 #define max(x,y)    ((x) > (y)? (x) : (y))
 #define min(x,y)    ((x) < (y)? (x) : (y))
@@ -68,9 +72,11 @@ int iii, jjj;
 
 
 /*@ begin Loop(
-  transform Composite(
-    tile = [('i',T1_I,'ii'),('j',T1_J,'jj'),(('ii','i'),T2_I,'iii'),(('jj','j'),T2_J,'jjj')],
-    unrolljam = (['i','j'],[U_I,U_J])
+transform Composite(
+      tile = [('i',T2_I,'ii'),('j',T2_J,'jj'),
+             (('ii','i'),T2_Ia,'iii'),(('jj','j'),T2_Ja,'jjj')],
+      unrolljam = (['i','j'],[U2_I,U2_J]),
+      regtile = (['i','j'],[RT2_I,RT2_J])
 )
 for (i=0;i<=N-1;i++)
   for (j=0;j<=N-1;j++) 
