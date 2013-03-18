26884895ad7b419b40f27f87372f81b2489533e3
pbalapra <pbalapra@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2013-03-18 21:14:35 +0000

    validation files
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@1308 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- testsuite/SPAPT/jacobi/validation.c
+++ testsuite/SPAPT/jacobi/validation.c
@@ -1,19 +1,21 @@
 
 int isValid() {
-  
-  double actual = 154057935.585664;
-
-  double ax_sum = 0.0;
+  double actual = 145057485.810668; 
+  double s_sum = 0.0;
+  double q_sum = 0.0;
   double rand1=0.1, rand2=0.9;
   double expected=0.0;
+  int i,j;
   double diff=0.0;
-  int i, j;
 
-  for (i=0; i<T; i++) 
-    for (j=0; j<N; j++) 
-      ax_sum+=a[i][j]*rand1*rand2;
 
-  expected = ax_sum;
+
+  for (i=1; i<=T-1; i++) 
+    for (j=1; j<=N-2; j++) 
+      s_sum+=a[i][j]*rand1*rand2;
+
+
+  expected = s_sum;
 
   diff=abs(expected-actual);
 
@@ -26,7 +28,6 @@ int isValid() {
     return 1;
   else
     return 0;
-
 }
 
 
