ad074942357669564f6c0b2420fed41957dfc544
pbalapra <pbalapra@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2013-03-18 21:14:45 +0000

    validation files

--- testsuite/SPAPT/atax/validation.c
+++ testsuite/SPAPT/atax/validation.c
@@ -1,12 +1,33 @@
 
 int isValid() {
   
-  double actual = 3.14159; // or some other user-defined computation
+  double actual = 9369846271501.798828; // or some other user-defined computation
+  double y_sum = 0.0;
+  double rand1=0.1, rand2=0.9;
+  double expected=0.0;
+  double diff=0.0;
+  int i;
+
+  for (i= 0; i<=ny-1; i++)
+    y_sum+=y[i]*rand1*rand2;
 
 
-  double expected = 3.14159;
-  if (expected==actual)
+  expected = y_sum;
+
+  diff=abs(expected-actual);
+
+  fprintf(stderr,"expected=%f\n",expected);
+  fprintf(stderr,"actual=%f\n",actual);
+  //printf("diff=%f\n",diff);
+  //printf("diff=%d\n",(diff < 0.00000001));
+
+  if (diff < 0.00000001)
     return 1;
   else
     return 0;
+
 }
+
+
+
+
