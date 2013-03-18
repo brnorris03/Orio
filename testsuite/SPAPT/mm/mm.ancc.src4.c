3680c0eb9b63145be18b1de8918973e07fc2f51a
pbalapra <pbalapra@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2013-03-18 21:16:06 +0000

    sources
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@1335 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- testsuite/SPAPT/mm/mm.ancc.src4.c
+++ testsuite/SPAPT/mm/mm.ancc.src4.c
@@ -22,22 +22,27 @@
     param T1_Ja[] = [1,64,128,256,512,1024,2048];
     param T1_Ka[] = [1,64,128,256,512,1024,2048];
 
-
-
     param U_I[] = range(1,31);
     param U_J[] = range(1,31);
     param U_K[] = range(1,31);
 
+    param RT_I[] = [1,8,32];
+    param RT_J[] = [1,8,32];
+    param RT_K[] = [1,8,32];
 
+    constraint tileI1 = ((T1_Ia == 1) or (T1_Ia % T1_I == 0));
+    constraint tileJ1 = ((T1_Ja == 1) or (T1_Ja % T1_J == 0));
+    constraint tileK1 = ((T1_Ka == 1) or (T1_Ka % T1_K == 0));
 
-
+    constraint reg_capacity = (RT_I*RT_J + RT_I*RT_K + RT_J*RT_K <= 150);
+    constraint unroll_limit = ((U_I == 1) or (U_J == 1) or (U_K == 1));
 
   }
 
   def search
   {
     arg algorithm = 'Randomsearch';
-    arg total_runs = 10000;
+    arg total_runs = 100000;
 
   }
   
@@ -56,13 +61,17 @@
     decl static double C[M][N] = 0;
   }            
 
+  def validation {
+    arg validation_file = 'validation.c';
+  }
+
 
 ) @*/
 
 int i, j, k;
 int ii, jj, kk;
 int iii, jjj, kkk;
-
+int it,jt,kt;
 #define max(x,y)    ((x) > (y)? (x) : (y))
 #define min(x,y)    ((x) < (y)? (x) : (y))
 
@@ -70,7 +79,8 @@ int iii, jjj, kkk;
   transform Composite(
     tile = [('i',T1_I,'ii'),('j',T1_J,'jj'),('k',T1_K,'kk'),
             (('ii','i'),T1_Ia,'iii'),(('jj','j'),T1_Ja,'jjj'),(('kk','k'),T1_Ka,'kkk')],
-    unrolljam = (['i','j','k'],[U_I,U_J,U_K])
+    unrolljam = (['i','j','k'],[U_I,U_J,U_K]),
+    regtile = (['i','j','k'],[RT_I,RT_J,RT_K])
 )
   for(i=0; i<=M-1; i++) 
     for(j=0; j<=N-1; j++)   
