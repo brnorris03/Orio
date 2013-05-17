2772de9d300e91a97b66aacd98fb20c936809758
azamat <azamat@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2013-05-17 00:12:40 +0000

    unroll unit tests

---
+++ testsuite/regression/src/unroll02.c
@@ -0,0 +1,15 @@
+void axpy(int n, double *y, double a, double *x) {
+  register int i;
+
+  /*@ begin Loop(transform Unroll(ufactor=5, parallelize=True)
+
+  for (i=0; i<=n-1; i++)
+    y[i]+=a*x[i];
+
+  ) @*/
+
+  for (i=0; i<=n-1; i++)
+    y[i]+=a*x[i];
+
+  /*@ end @*/
+}
