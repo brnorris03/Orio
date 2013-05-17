545bb6eabc16e329a3fca7bf0eca5fea939c3dfe
azamat <azamat@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2013-05-17 00:12:40 +0000

    unroll unit tests
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@1354 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

---
+++ testsuite/regression/ref/_unroll01.c
@@ -0,0 +1,22 @@
+void axpy(int n, double *y, double a, double *x) {
+  register int i;
+
+  /*@ begin Loop(transform Unroll(ufactor=5, parallelize=False)
+
+  for (i=0; i<=n-1; i++)
+    y[i]+=a*x[i];
+
+  ) @*/
+  {
+    for (i=0; i<=n-5; i=i+5) {
+      y[i]=y[i]+a*x[i];
+      y[(i+1)]=y[(i+1)]+a*x[(i+1)];
+      y[(i+2)]=y[(i+2)]+a*x[(i+2)];
+      y[(i+3)]=y[(i+3)]+a*x[(i+3)];
+      y[(i+4)]=y[(i+4)]+a*x[(i+4)];
+    }
+    for (i=n-((n-(0))%5); i<=n-1; i=i+1) 
+      y[i]=y[i]+a*x[i];
+  }
+/*@ end @*/
+}
