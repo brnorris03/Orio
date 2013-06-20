83d1717998d24513739744950dc5bf8f38be76ab
Azamat Mametjanov <azamat@mcs.anl.gov>
2013-06-20 17:50:41 +0000

    condensed milc qcd multi-matvec example
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@1383 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

--- testsuite/sandbox/cuda/milc_qcd/m_matvec.c
+++ testsuite/sandbox/cuda/milc_qcd/m_matvec.c
@@ -3,12 +3,11 @@
 * matrix vector multiply                                                *
 *  y[i]  <-  A[i]*x[i]                                                  *
 */
-void mult_su3_mat_vec(double A[], double x[], double y[]) {
-  const int sites_on_node = 10; // or some other global constant value
+void mult_su3_mat_vec(double A[], double x[], double y[], int sites_on_node) {
   register int i,j,k;
   register double ar,ai,br,bi,cr,ci;
 
-  /*@ begin PerfTuning (
+  /*@ PerfTuning (
         def performance_params {
           param TC[]  = range(32,1025,32);
           param BC[]  = range(14,113,14);
@@ -24,19 +23,10 @@ void mult_su3_mat_vec(double A[], double x[], double y[]) {
           decl dynamic double x[6*SITES]  = random;
           decl dynamic double y[6*SITES]  = 0;
         }
-        def build {
-          arg build_command = 'nvcc -arch=sm_20 @CFLAGS';
-        }
-        def performance_counter {
-          arg method = 'basic timer';
-          arg repetitions = 6;
-        }
   ) @*/
+  int sites_on_node = SITES;
 
-  int sites_on_node=SITES;
-
-  /*@ begin Loop(transform CUDA(threadCount=TC, blockCount=BC, preferL1Size=PL, unrollInner=UIF)
-
+  #pragma Orio Loop(transform CUDA(threadCount=TC, blockCount=BC, preferL1Size=PL, unrollInner=UIF))
   for(i=0; i<=sites_on_node-1; i++) {
     for(j=0; j<=5; j+=2) {
       cr = ci = 0.0;
@@ -52,24 +42,7 @@ void mult_su3_mat_vec(double A[], double x[], double y[]) {
       y[6*i+j+1]=ci;
     }
   }
-
-  ) @*/
-
-  for(i=0; i<=sites_on_node-1; i++) {
-    for(j=0; j<6; j+=2) {
-      cr = ci = 0.0;
-      for(k=0; k<6; k+=2) {
-        ar=A[18*i+3*j+k];
-        ai=A[18*i+3*j+k+1];
-        br=x[6*i+k];
-        bi=x[6*i+k+1];
-        cr += ar*br - ai*bi;
-        ci += ar*bi + ai*br;
-      }
-      y[6*i+j]  =cr;
-      y[6*i+j+1]=ci;
-    }
-  }
-  /*@ end @*/
-  /*@ end @*/
+  #pragma Oiro
+  /*@ @*/
 }
+
