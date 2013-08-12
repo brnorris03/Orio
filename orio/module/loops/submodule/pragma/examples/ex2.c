f1bab14002eb7b995bfd3d8f34be35a750170261
azamat <azamat@e7ad4b5f-b827-0410-872f-f7f4bc3d1efb>
2013-08-12 22:03:11 +0000

    cleanup

---
+++ orio/module/loops/submodule/pragma/examples/ex2.c
@@ -0,0 +1,6 @@
+
+#pragma Orio Loops(transform Pragma(pragma_str="vector always"))
+
+  S(i,j);
+
+#pragma Oiro
--- testsuite/sandbox/pragma2.c
+++
@@ -1,7 +0,0 @@
-
-/*@ begin Pragma('omp parallel for') @*/  
-for (i=0; i<n; i++)
-  y[i] = b[i] + ss*a[i];
-/*@ end @*/
-
-
