a75f2c911d4123579b864e52eac70756b3a76927
Azamat Mametjanov <azamat@mcs.anl.gov>
2013-10-25 21:43:26 +0000

    gcc simd tuning
    git-svn-id: https://svn.mcs.anl.gov/repos/performance/orio@1513 e7ad4b5f-b827-0410-872f-f7f4bc3d1efb

---
+++ orio/module/loops/submodule/pragma/examples/results/cg/vecWXPY/gcc/vecWXPY.c
@@ -0,0 +1,35 @@
+void add(int n, double *a, double *b, double *c) {
+
+/*@ begin PerfTuning(
+      def performance_params {
+        param PR[] = [ 
+                       "vector aligned",
+                       "vector unaligned",
+                       ""];
+        param CFLAGS[] = map(join, product(['', '-mavx']));
+      }
+      def build {
+        arg build_command = 'gcc -lrt -O3 @CFLAGS';
+      }
+      def input_params {
+        param N[] = [10**6,10**7,10**8,10**9];
+      }
+      def input_vars {
+        decl dynamic double a[N] = random;
+        decl dynamic double b[N] = random;
+        decl dynamic double c[N] = 0;
+      }
+) @*/
+
+  register int i;
+  int n=N;
+
+/*@ Loops(transform Pragma(pragma_str=PR)) @*/
+
+  for(i=0; i<=n; ++i) {
+    c[i] = a[i] + b[i];
+  }
+
+/*@ @*/
+/*@ @*/
+}
