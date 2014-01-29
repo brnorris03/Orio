e2c125dd22a46054333e7ac8715c6fed40433510
brnorris03 <brnorris03@gmail.com>
2014-01-29 13:01:31 -0800

    CHiLL module skeleton

--- orio/module/chill/chill.py
+++ orio/module/chill/chill.py
@@ -1,14 +1,12 @@
 #
-# This orio module does not perform any significant code transformations at all.
-# The purpose of this orio module is merely to provide a simple example to application developers
-# for how to extend Orio with a new program transformation orio.module.
+# This Orio 
 #
 
 import orio.module.module
 
 #-----------------------------------------
 
-class SimplyRewrite(orio.module.module.Module):
+class CHiLL(orio.module.module.Module):
     '''A simple rewriting module.'''
 
     def __init__(self, perf_params, module_body_code, annot_body_code, line_no, indent_size, language='C'):
@@ -33,7 +31,7 @@ class SimplyRewrite(orio.module.module.Module):
         */
         ''' % (self.perf_params, self.module_body_code, self.annot_body_code, self.line_no, self.indent_size)
 
-        # to rewrite the annotated code, with the class-attribute comment being prepended
+        # Do nothing except output the code annotated with a comment for the parameters that were specified
         output_code = comment + self.annot_body_code
 
         # return the output code
