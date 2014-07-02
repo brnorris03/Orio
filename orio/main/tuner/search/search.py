9765844bf2e81f31794553a3d3ffa8588f2b581e
brnorris03 <brnorris03@gmail.com>
2014-07-01 22:42:12 -0700

    fix indentation; use exception handling with TransformationException

--- orio/main/tuner/search/search.py
+++ orio/main/tuner/search/search.py
@@ -251,8 +251,11 @@ class Search:
 
             try:
                 transformed_code_seq = self.odriver.optimizeCodeFrags(self.cfrags, perf_params)
-            except Exception, e:
-                err('failed during evaluation of coordinate: %s=%s\n%s %s' % (coord, self.coordToPerfParams(coord), e.__class__.__name__, e), 0, False)
+            except TransformationException, e:
+                # Do not stop if a single test fails, continue with other transformations
+                err('failed during evaluation of coordinate: %s=%s\n%s\nError:%s' \
+                    % (str(coord), str(perf_params), str(e.__class__), e.message), 
+                    code=0, doexit=False)
                 perf_costs[coord_key] = ([self.MAXFLOAT],[self.MAXFLOAT])
                 continue
 
--- orio/main/util/matplotlib_logger.py
+++ orio/main/util/matplotlib_logger.py
@@ -1,30 +1,30 @@
 import logging
 
 class MatplotlibLogger:
-  def __init__(self, filename=None):
+    def __init__(self, filename=None):
     
-    if not filename: filename = 'results'
-    self.logfilename = filename
-    # Create the logger object
-    self.logger = logging.getLogger('TuningData')
-    self.logger.setLevel(logging.DEBUG)
-
-    # Create console handler and set level to info
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
+        if not filename: filename = 'results'
+        self.logfilename = filename
+        # Create the logger object
+        self.logger = logging.getLogger('TuningData')
+        self.logger.setLevel(logging.DEBUG)
     
-    # Custom formatter
-    formatter = logging.Formatter('#%(asctime)s\n%(message)s')
-    ch.setFormatter(formatter)
+        # Create console handler and set level to info
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        
+        # Custom formatter
+        formatter = logging.Formatter('#%(asctime)s\n%(message)s')
+        ch.setFormatter(formatter)
+        
+        # Add handler to logger
+        self.logger.addHandler(ch)
     
-    # Add handler to logger
-    self.logger.addHandler(ch)
+        # File name
+        self.logger.addHandler(logging.FileHandler(filename))
 
-    # File name
-    self.logger.addHandler(logging.FileHandler(filename))
-
-  def getLogger(self):
-    return self.logger
+    def getLogger(self):
+        return self.logger
 
 
 
