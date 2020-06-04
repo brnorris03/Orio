e79649cd8b2b07a7cedb8003dafc99abbb042226
Boyana Norris <brnorris03@gmail.com>
2020-06-03 21:17:42 -0700

    pragma node handling added

--- orio/module/loop/submodule/unrolljam/transformation.py
+++ orio/module/loop/submodule/unrolljam/transformation.py
@@ -224,6 +224,9 @@ class Transformation:
         elif isinstance(tnode, orio.module.loop.ast.Comment):
             return binOpExprs
 
+        elif isinstance(tnode, orio.module.loop.ast.Pragma):
+            return binOpExprs
+
         else:
             err('orio.module.loop.submodule.unrolljam.transformation.__analyzeForNewVars internal error: unexpected AST type: "%s"' % tnode.__class__.__name__)
             
