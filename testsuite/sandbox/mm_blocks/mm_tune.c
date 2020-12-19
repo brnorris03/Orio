932a398f9dcad86a62bcc7b3ad4a4f25b03156ab
Boyana Norris <brnorris03@gmail.com>
2020-12-19 10:28:10 -0800

    only consider square matrices

--- testsuite/sandbox/mm_blocks/mm_tune.c
+++ testsuite/sandbox/mm_blocks/mm_tune.c
@@ -42,20 +42,21 @@ void multiply_matrix_t(double** restrict mat_a, int rows_a, int cols_a,
       constraint unroll_limit = ((U_J == 1) or (U_K == 1));
     }
     def input_params {
-      let N = [4096, 8192, 16384];
-      #let N = [2048];
+      let N = [4096,8192,16384];
       param rows_a[] = N;
       param cols_a[] = N;
       param cols_b[] = N;
+
+      #constraint square = ((rows_a == cols_a) and (rows_a == cols_b));
     }
     def input_vars {
-    decl static double A[rows_a][cols_a] = random;
-    decl static double B[cols_b][cols_a] = random;
-    decl static double C[rows_a][cols_b] = random;
+      decl static double A[rows_a][cols_a] = random;
+      decl static double B[cols_b][cols_a] = random;
+      decl static double C[rows_a][cols_b] = random;
     }
     def search {
       arg algorithm = 'Mlsearch';
-      arg total_runs = 100;
+      arg total_runs = 20;
     }
   ) @*/
 
