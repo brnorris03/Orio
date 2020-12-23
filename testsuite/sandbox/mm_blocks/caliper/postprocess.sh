bc1af16cf99193d8c68815907c64e9e7c46f8a8f
Boyana Norris <brnorris03@users.noreply.github.com>
2020-12-22 18:47:20 -0800

    Update postprocess.sh
    comment explaining command-line argument expectations

--- testsuite/sandbox/mm_blocks/caliper/postprocess.sh
+++ testsuite/sandbox/mm_blocks/caliper/postprocess.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Expects two command-line arguments:
+#   $1: the name of the performance test executable, something like __orio_perftest2.exe
+#   $2: The string representation of the parameter coordinate indices, e.g., "[2, 0, 4]"
 
 #mv caliper.json caliper$1-"$2".log
 caliperdata="caliper.log"
